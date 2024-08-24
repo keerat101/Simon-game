a18a129de4a0819c68b3b4477377fc7590628206
keerat101 <141765456+keerat101@users.noreply.github.com>
2024-08-25 00:35:34 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Navbar=()=>{
+    return(
+        <> 
+        <button>  <a href="www.google.com">Hello</a></button>
+        </>
+    )
+}
+export default Navbar;
